7870c4fd46c3ff0260247424384e15286355ac00
BrouthenKamel <lk_brouthen@esi.dz>
2024-05-01 22:01:39 +0100

    Try updating google creds

--- app/setup/creds.py
+++ app/setup/creds.py
@@ -4,7 +4,6 @@ import dotenv
 dotenv.load_dotenv()
 
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-GOOGLE_APPLICATION_CREDENTIALS_PATH = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_PATH')
 
-def set_google_application_credentials():  
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS_PATH
+# def set_google_application_credentials():
+#     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = './app/setup/service_account.json'
